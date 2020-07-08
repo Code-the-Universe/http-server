0309efacb145b37b19e22df6930c66933e8ac002
mikadore <admin@mikadore.eu>
2020-07-08 18:20:01 +0200

    Got the main working kinda, but the HTTP is invalid

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <SFML/Network.hpp>
 #include <iostream>
+#include <ctime>
 
 int main()
 {
-    sf::TcpListener server;
-    server.listen(6669);
+    constexpr unsigned short port = 27277;
+    sf::TcpListener listener{ };
+    auto result = listener.listen(port);
+    if(result != sf::Socket::Done)
+    {
+        std::cout << "Error binding!\n";
+        std::exit(1);
+    }
     while(true)
     {
         sf::TcpSocket client;
-        server.accept(client);
-        std::cout << "Connection received!" << std::endl;
-        std::string data = "HTTP/1.1 501 Not Implemented";
-        client.send(data.data(), data.size() + 1);
+        result = listener.accept(client);
+        if (result != sf::Socket::Done) {
+            std::cout << "Error accepting client! \n";
+            std::exit(1);
+        } else {
+            std::cout << "Accepted client! \n";
+        }
+        std::string HTTP = "HTTP/1.1 501 NOT IMPLEMENTED \nDate: ";
+        std::time_t time = std::time(nullptr);
+        HTTP += std::asctime(std::localtime(&time));
+        std::size_t sent;
+        client.send(HTTP.c_str(), HTTP.size() + 1, sent);
+        std::cout << "Sent " << sent << " characters:\n" << HTTP;
     }
 }
